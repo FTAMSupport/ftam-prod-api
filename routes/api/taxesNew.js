9c6f864cc94247c9cde67c6ec1fd40e58d7e9ea3
skalapa <alaparthyshyam@yahoo.com>
2018-03-12 02:39:35 -0500

    updated config file

--- routes/api/taxesNew.js
+++ routes/api/taxesNew.js
@@ -4,22 +4,23 @@ var Restaurant = mongoose.model('Restaurant');
 var auth = require('../auth');
 const request = require('request-promise');
 var Avatax = require('avatax');
-var config = require('../../config');
+var tax_ava_login_id = require('../../config').tax_ava_login_id;
+var tax_ava_password = require('../../config').tax_ava_password;
 
 // resolve configuration and credentials 
-const ava_config = {
-  appName: 'ftam',
+const config = {
+  appName: 'your-app',
   appVersion: '1.0',
-  environment: 'production',
-  machineName: 'ftam-prod-api'
+  environment: 'sandbox',
+  machineName: 'your-machine-name'
 };
 
 const creds = {
-  username: config.tax_ava_login_id,
-  password: config.tax_ava_password
+  username: tax_ava_login_id,
+  password: tax_ava_password
 };
 
-var client = new Avatax(ava_config).withSecurity(creds);
+var client = new Avatax(config).withSecurity(creds);
 
 // Look up associated Tax price
 router.post('/lookup', function (req, res, next) {
@@ -30,7 +31,9 @@ router.post('/lookup', function (req, res, next) {
       // response tax document
       console.log(result);
       return res.json(result);
-    });
+    }).catch(function(e) {
+      console.log(e); 
+    })
 });
 
 module.exports = router;
